c4bedf135db86195a1a04b978c5b5f22eb99cba7
Sumbul Kaynat <sumbulkaynat@gmail.com>
2020-10-22 14:38:50 +0530

    ejs

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,8 @@ const homeController = require('../controllers/home_controller');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
 
+//for any further routes, access from here
+// router.use('/routerName', require('./routerfile'));
+
 module.exports = router;
 
