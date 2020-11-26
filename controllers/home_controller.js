bacad44fc7d2a792be8fd791206797bf5601bf10
Sumbul Kaynat <sumbulkaynat@gmail.com>
2020-11-26 15:14:31 +0530

    database relations

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,21 @@
 //module.exports.actionName = function(req,res){}
 
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    return res.render('home',{
-        title: "Home"
-    });
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: "ShareEmo | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        //if(err){console.log(err);return}
+        return res.render('home',{
+            title: "ShareEmo | Home",
+            posts: posts
+        });
+    })
 }
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -13,6 +13,10 @@ module.exports.userProfile = function(req,res){
 
 //render the sign-up page
 module.exports.signUp =  function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up',{
         title: "SocialMedia | Sign Up"
     });
@@ -20,6 +24,10 @@ module.exports.signUp =  function(req,res){
 
 //render the sign-in page
 module.exports.signIn =  function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in',{
         title: "SocialMedia | Sign In"
     });
@@ -48,5 +56,11 @@ module.exports.create =  function(req,res){
 
 //sign in and create a session for the user
 module.exports.createSession =  function(req,res){
-    //TODO Later
+    return res.redirect('/users/profile');
 } 
+
+module.exports.destroySession = function(req,res){
+    req.logout();
+
+    return res.redirect('/');
+}
